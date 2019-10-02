461ac74a82f663f675883279e74297398cbfafbf
swathi <shettys960@gmail.com>
2019-10-02 16:39:10 +0530

    fullstack

---
+++ app.js
@@ -0,0 +1,13 @@
+var name='anu';
+var name1='manu';
+var name2="anu and manu";
+console.log(typeof name);
+console.log(typeof name1);
+console.log(typeof name2);
+//number
+var num=10;
+var num1=10.1;
+var num2=1/1;
+console.log(num);
+console.log(num1);
+console.log(num2);
